6af6aeb02bfb2f15a2c1f0d589c7d228ed91c2e1
erictapen <justin.humm@posteo.de>
2015-09-28 02:07:26 +0200

    class Neuron

--- src/main/java/Neuron.java
+++ src/main/java/Neuron.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+
 
 public class Neuron {
+	ArrayList<Neuron> parents = new ArrayList<Neuron>();
+	
+	ArrayList<Neuron> children = new ArrayList<Neuron>();
+	
+	ArrayList<Double> weights = new ArrayList<Double>();
+	
+	double activation;
+	
+	double oldActivation;
+	
+	
+	public Neuron(double activation) {
+		super();
+		this.activation = activation;
+		this.oldActivation = 0.0;
+	}
 
+	public void update() {
+		//cumulate
+		double sum = 0.0;
+		for(int i=0; i<this.parents.size(); i++) {
+			sum += this.weights.get(i) * this.parents.get(i).getOldActivation();
+		}
+		//activate
+		this.activation = Math.tanh(sum);
+	}
+	
+	/** copies activation into oldActivation. Must be executed before any connected neuron update()
+	 * 
+	 */
+	public void prepareNextStep() {
+		this.oldActivation = this.activation;
+	}
+
+	public double getOldActivation() {
+		return oldActivation;
+	}
+	
+	
 }
